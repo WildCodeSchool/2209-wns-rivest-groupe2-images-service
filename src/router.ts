9893f05a1330c952d9c14c97961b8b166fad9848
charlinedev <charlinebarbelette@yahoo.fr>
2023-04-28 11:31:59 +0200

    add controller to router

--- src/router.ts
+++ src/router.ts
@@ -3,14 +3,16 @@ import { auth } from "./middlewares/auth"; // Crée un token d'identification
 import multer from "multer"; // Permet d'envoyer un fichier dans la requête
 import path from 'path';
 import { storage } from "./middlewares/multer-config";
+import Controller from './controller';
 
 //import upload from "./multer-config";
 
 const router = express.Router();
 
 // const upload = multer({ storage });
-router.post('/upload',auth, multer({ storage }).single('file'));
-router.get("/files/:filename");
+router.post('/upload',auth, multer({ storage }).single('file')),Controller.postImage;
+// router.get("/files/:filename"),Controller.getImage;
+router.get("/files/");
 router.put("/modify", auth, multer({ storage }).single('file'));
 router.delete('/:id', auth);
 
